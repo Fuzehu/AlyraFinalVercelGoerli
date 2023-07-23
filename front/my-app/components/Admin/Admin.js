9f64f4bec14969c4dd661fd60998d120dc46412c
Fuzehu <foti.n2a@gmail.com>
2023-07-23 17:21:46 +0200

    fixed bug for prod

--- front/my-app/components/Admin/Admin.js
+++ front/my-app/components/Admin/Admin.js
@@ -10,7 +10,7 @@ import { createPublicClient, http, parseAbiItem } from 'viem'
 import { readContract } from '@wagmi/core';
 import DiscountToken from '../../public/artifacts/contracts/DiscountToken.sol/DiscountToken.json'
 import Tokenize from '../../public/artifacts/contracts/Tokenize.sol/Tokenize.json'
-import StakingERC1155ID1 from '../../public/artifacts/contracts/StakingERC1155ID1.sol/StakingERC1155ID1.json'
+import StakingERC1155Id1 from '../../public/artifacts/contracts/StakingERC1155ID1.sol/StakingERC1155ID1.json'
 
 
 const Admin = () => {
@@ -50,7 +50,7 @@ const Admin = () => {
 
             const owner3 = await client.readContract({
                 address: contractAddress3,
-                abi: StakingERC1155ID1.abi,
+                abi: StakingERC1155Id1.abi,
                 functionName: "owner",
                 args: []
             });
